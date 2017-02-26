cbf3e7e3a386d01c157ff84cfb7c8a4fe12c4425
sba60 <sureyyabetul@hotmail.com>
2017-02-26 15:37:31 -0600

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,19 @@
+H=101
+H=101**2
+print(H)
+good=' Tea Time'
+print(str(H)+good)
+
+T="The funniest Task ever"
+print(len(T))
+print(T.index("n"))
+print(T.count("m"))
+print(T[::-4])
+print(T[2:11])
+print(T[-10:-6])
+print(T[-8:2])
+print(T.upper())
+print(T.lower())
+print(T.count("g"))
+print(T.count("l"))
+print(T.count('a'))
