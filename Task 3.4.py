f1215bc28fd3acb413531de852baa0cbda887546
sba60 <sureyyabetul@hotmail.com>
2017-04-04 11:00:02 -0500

    Update Task 3.4.py

--- Task 3.4.py
+++ Task 3.4.py
@@ -1,9 +1,14 @@
+raw_input = Enter two integers :
 x = 3
 y = 5
+
 	if x<6:
-		print (The result is: x+y  )
+		print (The result is: 15 )
 	else:
-		print (Do you want to continue (Y/N)? )
-print ("Hello Word ")
+		print (Do you want to continue (Y/N)?)
+	if  answer Y:
+		print (Enter two integers : )
+	else:
+	break
 
 		
